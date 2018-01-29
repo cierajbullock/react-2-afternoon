5d5bf7ddf4a3eede83e22ee7f8c0c242d426ccdf
Ciera <cierajbullock@Cieras-MBP.Home>
2018-01-29 15:25:54 -0700

    Stage 4 done

--- src/Stage 4/models/Employee.js
+++ src/Stage 4/models/Employee.js
@@ -1,5 +1,25 @@
 export default class Employee {
-  // constructor
+  constructor(id, name, phone, title) {
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.title = title;
+
+
+
+  }
+
+  updateName (str) {
+    this.name = str;
+  }
+
+  updatePhone (str) {
+    this.phone = str;
+  }
+
+  updateTitle (str) {
+    this.title = str;
+  }
   
   // updateName
 
